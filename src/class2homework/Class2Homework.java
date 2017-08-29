fb9242bc7480f30bb590ad59a6c1fd88d5a8f559
Isaac <Isaac@ISAAC_HP_OMEN>
2017-08-29 16:00:25 -0500

    Remove unnecessary auto-generated comments

---
+++ src/class2homework/Class2Homework.java
@@ -0,0 +1,16 @@
+package class2homework;
+
+/**
+ *
+ * @author Isaac
+ */
+public class Class2Homework {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
