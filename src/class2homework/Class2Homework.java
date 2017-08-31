b2064f5c11b22fdb3c4d9bf8f920b371fa0c891c
Isaac <Isaac@ISAAC_HP_OMEN>
2017-08-31 15:26:30 -0500

    Data and output for objects created

--- src/class2homework/Class2Homework.java
+++ src/class2homework/Class2Homework.java
@@ -10,5 +10,18 @@ public class Class2Homework {
         //create Person objects
         Person johnObject = new Person();
         Person maryObject = new Person();
+        
+        //manipulate data
+        johnObject.setFirstName("John");
+        johnObject.setLastName("Smith");
+        johnObject.setAge(43);
+        
+        maryObject.setFirstName("Mary");
+        maryObject.setLastName("Johnson");
+        maryObject.setAge(40);
+        
+        //output
+        System.out.println("Name: " + johnObject.getFirstName() + " " + johnObject.getLastName() + " Age: " + johnObject.getAge());
+        System.out.println("Name: " + maryObject.getFirstName() + " " + maryObject.getLastName() + " Age: " + maryObject.getAge());
     }
 }
