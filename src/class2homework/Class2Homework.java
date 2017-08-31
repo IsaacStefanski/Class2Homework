5ee9daead970ca50b8c7c2fe38ed26ab2630227a
Isaac <Isaac@ISAAC_HP_OMEN>
2017-08-31 15:20:50 -0500

    Made sure Person had its own class

--- src/class2homework/Class2Homework.java
+++ src/class2homework/Class2Homework.java
@@ -5,32 +5,7 @@ package class2homework;
  * @author Isaac
  */
 public class Class2Homework {
-
-    private String firstName;
-    private String lastName;
-    private int age;    
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    public static void main(String [] args){
+        
     }
 }
